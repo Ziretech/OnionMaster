22e872ceaa44fbb825f7992d4abc69300028f8db
Ziretech <josef.hoglund@gmail.com>
2017-01-05 15:14:58 +0100

    Lade till placeholder för programicon.

--- OnionMaster/Properties/Resources.Designer.cs
+++ OnionMaster/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace OnionMaster.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon OnionMaster {
+            get {
+                object obj = ResourceManager.GetObject("OnionMaster", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Resources\.
         /// </summary>
         internal static string ResourceFolder {
