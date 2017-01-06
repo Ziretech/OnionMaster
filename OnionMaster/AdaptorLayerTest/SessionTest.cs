97ceef559c43db4e45a8ccd13b4ef9e3fafe18c8
Ziretech <josef.hoglund@gmail.com>
2017-01-06 09:07:07 +0100

    Applikationsinställningar flyttade till Settings så att de kan förändras utan att man behöver bygga om projekten. (Man kan ändra vilket sessiondata och texture-fil som används.)

--- OnionMaster/AdaptorLayerTest/SessionTest.cs
+++ OnionMaster/AdaptorLayerTest/SessionTest.cs
@@ -22,7 +22,7 @@ namespace AdaptorLayer
         [TestMethod]
         public void Should_exit_game_when_escape_is_pressed()
         {
-            var session = new Session("", new InputMock(true, false, false, false, false), _useCasesMock);
+            var session = new Session(null, new InputMock(true, false, false, false, false), _useCasesMock);
             session.Update();
             Assert.IsTrue(session.ExitGame);
         }
@@ -30,7 +30,7 @@ namespace AdaptorLayer
         [TestMethod]
         public void Should_not_exit_game_when_escape_is_not_pressed()
         {
-            var session = new Session("", new InputMock(false, false, false, false, false), _useCasesMock);
+            var session = new Session(null, new InputMock(false, false, false, false, false), _useCasesMock);
             session.Update();
             Assert.IsFalse(session.ExitGame);
         }
@@ -38,7 +38,7 @@ namespace AdaptorLayer
         [TestMethod]
         public void Should_move_character_up_when_up_is_pressed()
         {
-            var session = new Session("", new InputMock(false, true, false, false, false), _useCasesMock);
+            var session = new Session(null, new InputMock(false, true, false, false, false), _useCasesMock);
             session.Update();
             Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveUpCalled);
         }
@@ -46,7 +46,7 @@ namespace AdaptorLayer
         [TestMethod]
         public void Should_move_character_down_when_down_is_pressed()
         {
-            var session = new Session("", new InputMock(false, false, true, false, false), _useCasesMock);
+            var session = new Session(null, new InputMock(false, false, true, false, false), _useCasesMock);
             session.Update();
             Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveDownCalled);
         }
@@ -54,7 +54,7 @@ namespace AdaptorLayer
         [TestMethod]
         public void Should_move_character_right_when_right_is_pressed()
         {
-            var session = new Session("", new InputMock(false, false, false, true, false), _useCasesMock);
+            var session = new Session(null, new InputMock(false, false, false, true, false), _useCasesMock);
             session.Update();
             Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveRightCalled);
         }
@@ -62,7 +62,7 @@ namespace AdaptorLayer
         [TestMethod]
         public void Should_move_character_left_when_left_is_pressed()
         {
-            var session = new Session("", new InputMock(false, false, false, false, true), _useCasesMock);
+            var session = new Session(null, new InputMock(false, false, false, false, true), _useCasesMock);
             session.Update();
             Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveLeftCalled);
         }
@@ -71,7 +71,7 @@ namespace AdaptorLayer
         public void Should_show_all_renderable_objects_when_draw_to_screen()
         {
             _useCasesMock.ShowAllRenderableObjectsMock.RenderInfos = new List<RenderInfo>() { new RenderInfo(1, 2, 3, 4, 5, 6, 7, 8) };
-            var session = new Session("", new InputMock(), _useCasesMock);
+            var session = new Session(null, new InputMock(), _useCasesMock);
             var commands = session.DrawScreen().ToList();
             Assert.AreEqual(1, commands.Count);
             Assert.AreEqual(new DrawCommand(1, 2, 4, 5, 6, 7, 8), commands[0]);
@@ -84,7 +84,7 @@ namespace AdaptorLayer
                 new RenderInfo(1, 9, 3, 9, 9, 9, 9, 9),
                 new RenderInfo(2, 9, 1, 9, 9, 9, 9, 9)
             };
-            var session = new Session("", new InputMock(), _useCasesMock);
+            var session = new Session(null, new InputMock(), _useCasesMock);
             var commands = session.DrawScreen().ToList();
             Assert.AreEqual(2, commands.Count);
             Assert.AreEqual(2, commands[0].ScreenX);
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # OnionMaster
 OpenTK RPG in C# .NET
-
-## Editera Session data förhand
-
-Resources/
