403d9aab2717b3db998871a5d78d76cc73e725c6
Ziretech <josef.hoglund@gmail.com>
2017-01-07 00:11:26 +0100

    Tester av session i AdaptorLayer konverterade till NUnit. Tog bort referenser till MSTest från AdoptorLayerTest och UseCaseLayerTest.

--- OnionMaster/AdaptorLayerTest/SessionTest.cs
+++ OnionMaster/AdaptorLayerTest/SessionTest.cs
@@ -1,83 +1,80 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using UseCaseLayer.Player;
-using EntityLayer;
+﻿using NUnit.Framework;
 using System.Collections.Generic;
 using UseCaseLayer.Rendering;
 using System.Linq;
 
 namespace AdaptorLayer
 {
-    [TestClass]
+    [TestFixture]
     public class SessionTest
     {
         private UseCaseProviderMock _useCasesMock;
 
-        [TestInitialize]
+        [SetUp]
         public void Setup()
         {
             _useCasesMock = new UseCaseProviderMock();
         }
 
-        [TestMethod]
+        [Test]
         public void Should_exit_game_when_escape_is_pressed()
         {
             var session = new Session(null, new InputMock(true, false, false, false, false), _useCasesMock);
             session.Update();
-            Assert.IsTrue(session.ExitGame);
+            Assert.That(session.ExitGame);
         }
 
-        [TestMethod]
+        [Test]
         public void Should_not_exit_game_when_escape_is_not_pressed()
         {
             var session = new Session(null, new InputMock(false, false, false, false, false), _useCasesMock);
             session.Update();
-            Assert.IsFalse(session.ExitGame);
+            Assert.That(!session.ExitGame);
         }
 
-        [TestMethod]
+        [Test]
         public void Should_move_character_up_when_up_is_pressed()
         {
             var session = new Session(null, new InputMock(false, true, false, false, false), _useCasesMock);
             session.Update();
-            Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveUpCalled);
+            Assert.That(_useCasesMock.MoveControlledCharacterMock.MoveUpCalled, Is.EqualTo(1));
         }
 
-        [TestMethod]
+        [Test]
         public void Should_move_character_down_when_down_is_pressed()
         {
             var session = new Session(null, new InputMock(false, false, true, false, false), _useCasesMock);
             session.Update();
-            Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveDownCalled);
+            Assert.That(_useCasesMock.MoveControlledCharacterMock.MoveDownCalled, Is.EqualTo(1));
         }
 
-        [TestMethod]
+        [Test]
         public void Should_move_character_right_when_right_is_pressed()
         {
             var session = new Session(null, new InputMock(false, false, false, true, false), _useCasesMock);
             session.Update();
-            Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveRightCalled);
+            Assert.That(_useCasesMock.MoveControlledCharacterMock.MoveRightCalled, Is.EqualTo(1));
         }
 
-        [TestMethod]
+        [Test]
         public void Should_move_character_left_when_left_is_pressed()
         {
             var session = new Session(null, new InputMock(false, false, false, false, true), _useCasesMock);
             session.Update();
-            Assert.AreEqual(1, _useCasesMock.MoveControlledCharacterMock.MoveLeftCalled);
+            Assert.That(_useCasesMock.MoveControlledCharacterMock.MoveLeftCalled, Is.EqualTo(1));
         }
         
-        [TestMethod]
+        [Test]
         public void Should_show_all_renderable_objects_when_draw_to_screen()
         {
             _useCasesMock.ShowAllRenderableObjectsMock.RenderInfos = new List<RenderInfo>() { new RenderInfo(1, 2, 3, 4, 5, 6, 7, 8) };
             var session = new Session(null, new InputMock(), _useCasesMock);
             var commands = session.DrawScreen().ToList();
-            Assert.AreEqual(1, commands.Count);
-            Assert.AreEqual(new DrawCommand(1, 2, 4, 5, 6, 7, 8), commands[0]);
+            Assert.That(commands.Count, Is.EqualTo(1));
+            Assert.That(commands[0], Is.EqualTo(new DrawCommand(1, 2, 4, 5, 6, 7, 8)));
         }
 
-        [TestMethod]
+        [Test]
         public void Should_sort_renderable_objects_by_layer()
         {
             _useCasesMock.ShowAllRenderableObjectsMock.RenderInfos = new List<RenderInfo>() {
@@ -86,9 +83,9 @@ namespace AdaptorLayer
             };
             var session = new Session(null, new InputMock(), _useCasesMock);
             var commands = session.DrawScreen().ToList();
-            Assert.AreEqual(2, commands.Count);
-            Assert.AreEqual(2, commands[0].ScreenX);
-            Assert.AreEqual(1, commands[1].ScreenX);
+            Assert.That(commands.Count, Is.EqualTo(2));
+            Assert.That(commands[0].ScreenX, Is.EqualTo(2));
+            Assert.That(commands[1].ScreenX, Is.EqualTo(1));
         }
     }
 }
