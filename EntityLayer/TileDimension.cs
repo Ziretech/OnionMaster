3238cd1606f4e9313adc183f880034dcf79d8d8f
Ziretech <josef.hoglund@gmail.com>
2017-01-08 20:51:49 +0100

    Tester för TileDimension.

--- EntityLayer/TileDimension.cs
+++ EntityLayer/TileDimension.cs
@@ -10,5 +10,27 @@
             Width = width;
             Height = height;
         }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && Equals(obj as TileDimension);
+        }
+
+        public bool Equals(TileDimension other)
+        {
+            return other != null &&
+                Width == other.Width &&
+                Height == other.Height;
+        }
+
+        public override int GetHashCode()
+        {
+            return Width ^ Height;
+        }
+
+        public override string ToString()
+        {
+            return $"{Width}x{Height}";
+        }
     }
 }
---
+++ EntityLayerTest/TileDimensionTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace EntityLayer
+{
+    [TestFixture]
+    public class TileDimensionTest
+    {
+        private TileDimension _dimension;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dimension = new TileDimension(1, 2);
+        }
+
+        [Test]
+        public void Should_have_a_constructor_to_set_width_height()
+        {
+            Assert.That(_dimension.Width, Is.EqualTo(1));
+            Assert.That(_dimension.Height, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Should_consider_two_coordinates_with_same_value_as_equal()
+        {
+            Assert.That(_dimension, Is.EqualTo(new TileDimension(1, 2)));
+        }
+
+        [Test]
+        public void Should_convert_to_string()
+        {
+            Assert.That(_dimension.ToString(), Is.EqualTo("1x2"));
+        }
+    }
+}
